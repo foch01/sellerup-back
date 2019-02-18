bbaeff2bdd2d5b3c56832952df64908a15776f0a
Foch01 <tsanceo@yahoo.com>
2019-02-18 12:34:34 +0100

    [Interface API - MWS] - Correctif

---
+++ src/util/mws/BuildRequestMWSUtils.ts
@@ -0,0 +1,42 @@
+import MwsApi from "amazon-mws";
+import { confShortNameCountry } from "../../constants/ConfMWSConstant";
+
+export class BuildRequestMWSUtils {
+
+    static getAllOrders = async (CreatedAfter: Date, marketplaces: any[]): Promise<any> => {
+        try {
+            const build: any[] = [
+                {
+                    "Version": "2013-09-01",
+                    "Action": "ListOrders",
+                    "SellerId": process.env.SELLER_ID,
+                    "MWSAuthToken": process.env.MWS_AUTH_TOKEN,
+                    "CreatedAfter": new Date(2019, 1, 17)
+                }
+            ];
+            let i = 1;
+            marketplaces.forEach((marketplace: string) => {
+                if (marketplace === confShortNameCountry.FR) {
+                    build[0][`MarketplaceId.Id.${i}`] = process.env.MARKETPLACE_ID_FR;
+                    i++;
+                } else if (marketplace === confShortNameCountry.DE) {
+                    build[0][`MarketplaceId.Id.${i}`] = process.env.MARKETPLACE_ID_DE;
+                    i++;
+                } else if (marketplace === confShortNameCountry.IT) {
+                    build[0][`MarketplaceId.Id.${i}`] = process.env.MARKETPLACE_ID_IT;
+                    i++;
+                } else if (marketplace === confShortNameCountry.UK) {
+                    build[0][`MarketplaceId.Id.${i}`] = process.env.MARKETPLACE_ID_UK;
+                    i++;
+                } else if (marketplace === confShortNameCountry.ES) {
+                    build[0][`MarketplaceId.Id.${i}`] = process.env.MARKETPLACE_ID_ES;
+                    i++;
+                }
+            });
+            return build[0];
+        } catch (e) {
+            console.log("error", e);
+        }
+    }
+
+}
