acd441905e35a7bd86b4b93837d6e5b474ae529e
Thomas <tsanceo@yahoo.com>
2019-02-19 14:45:24 +0000

    example interface api - mws

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
-
+import { MWSService } from "../services/MWSService";
+import MwsApi from "amazon-mws";
 /**
  * GET /
  * Home page.
  */
 export let index = (req: Request, res: Response) => {
-  res.render("home", {
+    res.render("home", {
     title: "Home"
   });
 };
