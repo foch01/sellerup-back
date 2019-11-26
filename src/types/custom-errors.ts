de9eb2e5e98119cb10ffa9977db85b722b3c41bc
MaolmeoX <arnaud.maurice.pro@gmail.com>
2019-11-26 16:38:52 +0100

    feat(login): add bad credentials error

---
+++ src/types/custom-errors.ts
@@ -0,0 +1,12 @@
+export class BadCredentialsError extends Error {
+    status: number;
+    message: string;
+    level: string;
+    constructor() {
+        const message = "Bad credentials";
+        super(message);
+        this.status = 400;
+        this.level = 'info'
+        this.message = message;
+    }
+  }
